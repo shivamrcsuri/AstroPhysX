99c739cbd70d58d30fa4b24b76233a8b0e3f0394
shivamrcsuri <65080692+shivamrcsuri@users.noreply.github.com>
2021-08-15 03:18:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, redirect, url_for
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+
+@app.route("/game")
+def game():
+  return render_template("game.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
